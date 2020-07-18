1bcc0bc4940696f007c2be910c4fbe5f883a7681
RejowanHossain <rezwan.hossain@outlook.com>
2020-07-18 20:16:50 +0600

    solved menu issues

--- js/app.js
+++ js/app.js
@@ -1,3 +1,39 @@
-setTimeout(function () {
-    $('.loader-bg').fadeToggle();
-}, 2200);
+(function ($) {
+    "use strict";
+
+    // sticky
+    // $(window).on('scroll', function () {
+    //     var scroll = $(window).scrollTop();
+    //     if (scroll < 2) {
+    //         $("#header-sticky").removeClass("sticky");
+    //     } else {
+    //         $("#header-sticky").addClass("sticky");
+    //     }
+    // });
+    setTimeout(function () {
+        $('.loader-bg').fadeToggle();
+    }, 2200);
+
+
+
+    $('#mobile-menu').meanmenu({
+        meanMenuContainer: '.mobile-menu',
+        meanScreenWidth: "991"
+    });
+
+
+    // $('.service-active').slick({
+    //     infinite: true,
+    //     slidesToShow: 2,
+    //     slidesToScroll: 1
+    // });
+
+
+
+    $('select').niceSelect();
+
+
+
+
+
+})(jQuery);
